864997952bb0c878a4166262590d81f6a9c30389
allydevonly <alibouadel2@gmail.com>
2023-10-03 02:49:45 +0100

    GMC_ethics_update

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function Test_Page(){   
   let Mytitle = document.getElementById("title_1")
-Mytitle.innerHTML = "js title"
-alert('press okay to continue');
+Mytitle.innerHTML = "GoMyCode workspace rules"
 }
 
 let myjsbutton = document.getElementById('js_btn')
-myjsbutton.addEventListener('click',Test_Page );
+myjsbutton.addEventListener('click',RBTN1 );
+myjsbutton.addEventListener('click',RBTN2 );
+
+   function RBTN1 (){
+    let Rbutton1 = document.getElementById("my_rules1")
+    Rbutton1.innerHTML = "Stay Organized"
+    let Rbutton2 = document.getElementById("my_rules2")
+  Rbutton2.innerHTML = "Practice Critical Thinking"
+  let Rbutton3 = document.getElementById("my_rules3")
+  Rbutton3.innerHTML = "Collaborate with Peers"
+  let Rbutton4 = document.getElementById("my_rules4")
+      Rbutton4.innerHTML = "Engage Actively in Class"
+    }
+
+ 
+    
+      
+       
